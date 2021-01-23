59c92e863346b2812bc4fad6ace6364a77ceabca
kahmnkk <kyuhh1214@naver.com>
2021-01-23 18:32:21 +0900

    add. project setting

---
+++ config.js
@@ -0,0 +1,37 @@
+const config = {
+    dev: true,
+    baseUrl: '127.0.0.1',
+    port: 3000,
+    mysql: {
+        master: {
+            host: '127.0.0.1',
+            port: 3306,
+            user: 'root',
+            password: '1234',
+            database: 'db_master',
+            waitForConnections: true,
+            connectionLimit: 10,
+            queueLimit: 0,
+            multipleStatements: true,
+        },
+    },
+    redis: {
+        sessionStore: {
+            host: '127.0.0.1',
+            port: 6379,
+            db: 0,
+        },
+        gen: {
+            host: '127.0.0.1',
+            port: 6380,
+            db: 0,
+        },
+        user: {
+            host: '127.0.0.1',
+            port: 6379,
+            db: 0,
+        },
+    },
+};
+
+module.exports = config;
---
+++ definePrototype.js
@@ -0,0 +1,86 @@
+// https://github.com/ilearnio/module-alias
+require('module-alias/register');
+
+Object.defineProperty(Array.prototype, 'select', {
+    value: (closure) => {
+        for (let i = 0; i < this.length; ++i) {
+            if (closure(this[i])) {
+                return this[i];
+            }
+        }
+        return null;
+    },
+});
+
+Object.defineProperty(Object.prototype, 'deepFreeze', {
+    value: (object) => {
+        var propNames = Object.getOwnPropertyNames(object);
+
+        for (let name of propNames) {
+            let value = object[name];
+
+            object[name] = value && typeof value === 'object' ? Object.deepFreeze(value) : value;
+        }
+
+        return Object.freeze(object);
+    },
+});
+
+Object.defineProperty(Object.prototype, 'mergeIntersect', {
+    value: (to, from) => {
+        for (let k in from) {
+            if (to.hasOwnProperty(k) == true) {
+                to[k] = from[k];
+            }
+        }
+
+        return to;
+    },
+});
+
+Object.defineProperty(Object.prototype, 'mergeIntersectWithCalc', {
+    value: (to, from) => {
+        for (let k in from) {
+            if (to.hasOwnProperty(k) == true) {
+                let v = from[k];
+                if (v != null && v.constructor.name == 'String') {
+                    const regexpCalc = new RegExp('^(?:[0-9]+\\s*[+\\-*/]\\s*' + k + '|' + k + '\\s*[+\\-*/]\\s*[0-9]+)$');
+                    const matches = v.match(regexpCalc);
+                    if (matches != null) {
+                        v = v.replace(k, to[k]);
+                        v = eval(v);
+                    }
+                }
+                to[k] = v;
+            }
+        }
+
+        return to;
+    },
+});
+
+Object.defineProperty(global, '__stack', {
+    get: function () {
+        var orig = Error.prepareStackTrace;
+        Error.prepareStackTrace = function (_, stack) {
+            return stack;
+        };
+        var err = new Error();
+        Error.captureStackTrace(err, arguments.callee);
+        var stack = err.stack;
+        Error.prepareStackTrace = orig;
+        return stack;
+    },
+});
+
+Object.defineProperty(global, '__line', {
+    get: function () {
+        return __stack[1].getLineNumber();
+    },
+});
+
+Object.defineProperty(global, '__function', {
+    get: function () {
+        return __stack[1].getFunctionName();
+    },
+});
